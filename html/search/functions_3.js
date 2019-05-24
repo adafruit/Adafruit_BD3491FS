0cdc97407a4707af19dabe70be43d3cf6755213d
Travis CI <travis@travis-ci.org>
2019-05-24 19:48:38 +0000

    Deploy code docs to GitHub Pages Travis build: 5
    Commit: f5ab9f72f11e3761790d62afce1432c967ead0a5

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -4,6 +4,6 @@ var searchData=
   ['setcurrentinput',['setCurrentInput',['../class_adafruit___b_d3491_f_s.html#ae64471ffe2b3947cd3a4bd2324004b39',1,'Adafruit_BD3491FS']]],
   ['setinputgain',['setInputGain',['../class_adafruit___b_d3491_f_s.html#a6be619adb56d650e92ae288bd37fcfac',1,'Adafruit_BD3491FS']]],
   ['settreblegain',['setTrebleGain',['../class_adafruit___b_d3491_f_s.html#a6d91e6e3562811b99d978939db21cb7b',1,'Adafruit_BD3491FS']]],
-  ['setvolumech1',['setVolumeCh1',['../class_adafruit___b_d3491_f_s.html#aa9aa6a621755b4ecdfd984f670bcfdb9',1,'Adafruit_BD3491FS']]],
-  ['setvolumech2',['setVolumeCh2',['../class_adafruit___b_d3491_f_s.html#ac68528bca7f044953798b2fa69e530ef',1,'Adafruit_BD3491FS']]]
+  ['setvolumech1',['setVolumeCh1',['../class_adafruit___b_d3491_f_s.html#a779224134dece4cff0f11170d62e72fa',1,'Adafruit_BD3491FS']]],
+  ['setvolumech2',['setVolumeCh2',['../class_adafruit___b_d3491_f_s.html#a9f808e046fdbb0461bd205c4012bccaf',1,'Adafruit_BD3491FS']]]
 ];
